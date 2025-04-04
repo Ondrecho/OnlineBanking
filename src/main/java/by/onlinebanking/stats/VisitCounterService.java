75f2af538379178c85aa10259460414225cf4d90
Андрей Шевченко <2908ash@gmail.com>
2025-04-04 17:54:08 +0300

    add visit statistics

---
+++ src/main/java/by/onlinebanking/stats/VisitCounterService.java
@@ -0,0 +1,21 @@
+package by.onlinebanking.stats;
+
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitCounterService {
+    private final VisitStats visitStats = new VisitStats();
+
+    public synchronized void recordVisit(String url) {
+        visitStats.recordVisit(url);
+    }
+
+    public synchronized long getVisitCount(String url) {
+        return visitStats.getVisitCount(url);
+    }
+
+    public synchronized Map<String, Long> getAllStats() {
+        return visitStats.getAllStats();
+    }
+}
